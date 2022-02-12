ca6fcc4d4f0da475f44dbab42154b6289a1fd577
AniRay-Project <99536949+AniRay-Project@users.noreply.github.com>
2022-02-12 14:26:23 +0300

    lib

---
+++ lib/module/ui-main-footer.php
@@ -0,0 +1,131 @@
+<footer>
+<div class="footer-con-container">
+	<div class="footer-con-overlay darkmode-header">
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+			  <div class="wavebar-svg-object"></div>
+			  <div class="wavebar-svg-object"></div>
+			</div>
+		</div>
+		<div class="footer-con-header">
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class="footer-ico-logo darkmode-footer-logo">
+					</div>
+					<table class="footer-table-out">
+					<tr>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Main</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://vk.com/firstpeoplestory">
+										<span>Sponsor</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="/about">
+										<span>About</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="/disclaimer">
+										<span>Disclaimer</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Support</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://vk.com/Toro_nl" target="_blank">
+										<span>AniRay</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://vk.com/id363123524" target="_blank">
+										<span>Менеджер</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://vk.com/id209397264" target="_blank">
+										<span>гл.Помощник</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://vk.com/k666sky666k" target="_blank">
+										<span>Помощник</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Social</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://discord.gg/XaNED9Yqu9" target="_blank">
+										<span>Discord</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://www.youtube.com/channel/UCf6Jj3Piqg9NCA_1MS5i_ZA" target="_blank">
+										<span>YouTube</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://twitter.com/" target="_blank">
+										<span>Twitter</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://vk.com/AniRayProject" target="_blank">
+										<span>AniRay Public</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td class="footer-con-sources">
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Forum</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="405" target="_blank">
+										<span>Forum</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+					</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class='footer-con-credits'>
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class="footer-con-foot">
+						<div class="footer-tx2-developer">
+							<span>Основано Toro © 2021</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
